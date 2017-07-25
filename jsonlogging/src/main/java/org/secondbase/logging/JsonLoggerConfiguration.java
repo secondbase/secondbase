2590ca9e575e1742247db35c85d3ad012c617238
Robert Neumayer <neumayer@users.noreply.github.com>
2017-07-25 14:02:39 +0200

    Add request logger name parameter to json logging (#36)
    * Add request logger name parameter to json logging
    
    * Fix line breaks
    
    * Fix imports and class dependencies

---
+++ jsonlogging/src/main/java/org/secondbase/logging/JsonLoggerConfiguration.java
@@ -0,0 +1,30 @@
+package org.secondbase.logging;
+
+import org.secondbase.flags.Flag;
+
+public class JsonLoggerConfiguration {
+
+    @Flag(
+            name = "environment",
+            description = "The environment the service runs in (test|staging|production)."
+    )
+    public static String environment;
+
+    @Flag(
+            name = "service",
+            description = "The name of the service (testservice)."
+    )
+    public static String service;
+
+    @Flag(
+            name = "datacenter",
+            description = "The datacenter the service runs in (dc1|dc2)."
+    )
+    public static String datacenter;
+
+    @Flag(
+            name = "request-logger-class-name",
+            description = "Name of the request logger class."
+    )
+    public static String requestLoggerClassName;
+}
--- jsonlogging/src/main/java/org/secondbase/logging/JsonLoggerModule.java
+++ jsonlogging/src/main/java/org/secondbase/logging/JsonLoggerModule.java
@@ -2,33 +2,23 @@ package org.secondbase.logging;
 
 import org.secondbase.core.SecondBase;
 import org.secondbase.core.config.SecondBaseModule;
-import org.secondbase.flags.Flag;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class JsonLoggerModule implements SecondBaseModule {
 
-    @Flag(name = "environment",
-            description = "The environment the service runs in (test|staging|production).", required = false)
-    public static String environment;
-
-    @Flag(name = "service", description = "The name of the service (testservice).", required = false)
-    public static String service;
-
-    @Flag(name = "datacenter",
-            description = "The datacenter the service runs in (dc1|dc2).", required = false)
-    public static String datacenter;
-
     @Override
     public void load(final SecondBase secondBase) {
-        secondBase.getFlags().loadOpts(JsonLoggerModule.class);
+        secondBase.getFlags().loadOpts(JsonLoggerConfiguration.class);
     }
 
     @Override
     public void init() {
         SLF4JBridgeHandler.removeHandlersForRootLogger();
         SLF4JBridgeHandler.install();
-        final String nullRequestLoggerName = null;
-        SecondBaseLogger.setupLoggingStdoutOnly(JsonLoggerModule.environment,
-                JsonLoggerModule.service, JsonLoggerModule.datacenter, nullRequestLoggerName);
+        SecondBaseLogger.setupLoggingStdoutOnly(
+                JsonLoggerConfiguration.environment,
+                JsonLoggerConfiguration.service,
+                JsonLoggerConfiguration.datacenter,
+                JsonLoggerConfiguration.requestLoggerClassName);
     }
 }
